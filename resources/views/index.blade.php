d61ef5479dc7619b648937a62c2158d14e204cbf
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-05-18 17:46:51 +0200

    little change

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,54 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/0510089784.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/app.css">
     <title>Comics</title>
-    <style scoped>
- ul {
-  list-style-type: none;
-}
-li {
-  padding: 0px 10px;
-  display: inline;
-}
-.bg-blue {
-  background-color: hsl(209deg 98% 49%);
-}
-.c-grey {
-  color: grey;
-}
-.c-red {
-  color: red;
-}
-.c-white {
-  color: white !important;
-}
-a {
-  text-decoration: none !important;
-}
-a:hover {
-  color: grey !important;
-}
-.c-white{
-color: white;
-}
-.c-blue{
-  color: hsl(209deg 98% 49%);
-}
-.space {
- height: 30px;
-  background-color: hsl(209deg 98% 49%);
-        }
-        ul{
-            list-style-type: disc;
-        }
-        li {
-            d
-          font-weight: 700;
-        }
-        header .container-fluid {
-          padding: 0;
-        }
-        </style>
 </head>
 <body>
     <header>
@@ -69,22 +23,32 @@ color: white;
                 <div class="col-2">
                   <img src="../images/dc-logo.png" alt="dc-logo.png" />
                 </div>
-                <div class="col-10 d-flex align-items-center justify-content-end">
-                  <ul>
+                <div class="col-8 d-flex align-items-center justify-content-end">
+                  <ul class="header-list">
                     <li>CHARACTERS</li>
-                    <li>COMICS</li>
+                    <li>COMICS </li>
                     <li>MOVIES</li>
                     <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
                     <li>VIDEOS</li>
                     <li>FANS</li>
                     <li>NEWS</li>
-                    <li>SHOP <i class="fas fa-caret-down c-blue"></li>
+                    <li>SHOP<i class="fas fa-caret-down c-blue"></li>
                   </ul>
+                </div>
+                <div class="col-8 d-flex">
+                    
                 </div>
               </div>
             </div>
           </div>
     </header>
+    <main>
+        <div>
+            <div class="container-fluid jt-image">
+            </div>           
+    </main>
     
 </body>
 </html>
