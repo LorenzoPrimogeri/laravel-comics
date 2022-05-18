5b5d31094285d15f731c1365b6e90bbb492a1233
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-05-18 17:11:03 +0200

    first push

---
+++ resources/views/index.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/0510089784.js" crossorigin="anonymous"></script>
+    <title>Comics</title>
+    <style scoped>
+ ul {
+  list-style-type: none;
+}
+li {
+  padding: 0px 10px;
+  display: inline;
+}
+.bg-blue {
+  background-color: hsl(209deg 98% 49%);
+}
+.c-grey {
+  color: grey;
+}
+.c-red {
+  color: red;
+}
+.c-white {
+  color: white !important;
+}
+a {
+  text-decoration: none !important;
+}
+a:hover {
+  color: grey !important;
+}
+.c-white{
+color: white;
+}
+.c-blue{
+  color: hsl(209deg 98% 49%);
+}
+.space {
+ height: 30px;
+  background-color: hsl(209deg 98% 49%);
+        }
+        ul{
+            list-style-type: disc;
+        }
+        li {
+            d
+          font-weight: 700;
+        }
+        header .container-fluid {
+          padding: 0;
+        }
+        </style>
+</head>
+<body>
+    <header>
+        <div class="container-fluid space bg-blue">
+            <div class="container ">
+                <ul class="c-white d-flex justify-content-end">
+                    <li class="px-2">DC POWER VISA</li>
+                    <li>ADDITIONAL DC SITES <i class="fas fa-caret-down "></i></li>
+                </ul>
+            </div>
+            <div class="container">
+              <div class="row justify-content-betweeen py-3">
+                <div class="col-2">
+                  <img src="../images/dc-logo.png" alt="dc-logo.png" />
+                </div>
+                <div class="col-10 d-flex align-items-center justify-content-end">
+                  <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP <i class="fas fa-caret-down c-blue"></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+    </header>
+    
+</body>
+</html>
